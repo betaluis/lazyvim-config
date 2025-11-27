4be86f196b9ee3d130a8eeac991b245df8b12bfc
Luis Betancourt <luis.betancourt.dev@gmail.com>
2025-11-27 21:02:00 +0000

    relative numbers in netrw

--- lua/config/core/autocommands.lua
+++ lua/config/core/autocommands.lua
@@ -2,20 +2,20 @@ local autocommand = vim.api.nvim_create_autocmd
 local keymap = vim.keymap.set
 
 -- Use 'q' to quit from common plugins
-autocommand({ 'Filetype' }, {
-    pattern = { 'qf', 'help', 'man', 'lspinfo', 'spectre_panel', 'lir' },
-    callback = function()
-        vim.cmd [[
+autocommand({ "Filetype" }, {
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
+	callback = function()
+		vim.cmd([[
             noremap <silent> <buffer> q :close<CR>
             set nobuflisted
-        ]]
-    end,
+        ]])
+	end,
 })
 
 autocommand({ "Filetype" }, {
-    pattern = { "netrw" },
-    callback = function()
-        vim.cmd [[
+	pattern = { "netrw" },
+	callback = function()
+		vim.cmd([[
             nmap <buffer> H u
             nmap <buffer> h -^
             nmap <buffer> l <CR>
@@ -23,21 +23,28 @@ autocommand({ "Filetype" }, {
             nmap <buffer> P <C-w>z
             setlocal number
 
-        ]]
-    end
+        ]])
+	end,
 })
 
 -- Mail
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = {"*.txt", "*.md", "mail"},
-  command = "setlocal spell",
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	pattern = { "*.txt", "*.md", "mail" },
+	command = "setlocal spell",
 })
 
 -- Yank Highlight
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = "netrw",
+	callback = function()
+		vim.opt_local.relativenumber = true
+	end,
 })
