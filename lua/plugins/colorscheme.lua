7eb139112876ab02315063f980079a4e5b8f7d7c
Luis Betancourt <luis.betancourt.dev@gmail.com>
2026-05-05 07:44:43 -0500

    markdown

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,77 +1,77 @@
 function ColorMyPencils(color)
-	color = color or "everforest"
-	vim.cmd.colorscheme(color)
+    color = color or "everforest"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(1, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(1, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(1, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(1, "NormalFloat", { bg = "none" })
 end
 
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"neanias/everforest-nvim",
-		name = "everforest",
-		config = function()
-			ColorMyPencils()
-		end,
-	},
-	{ "shaunsingh/nord.nvim" },
-	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
-	{
-		"loctvl842/monokai-pro.nvim",
-		config = function()
-			require("monokai-pro").setup({
-				transparent_background = false,
-				terminal_colors = true,
-				devicons = true, -- highlight the icons of `nvim-web-devicons`
-				styles = {
-					comment = { italic = true },
-					keyword = { italic = true }, -- any other keyword
-					type = { italic = true }, -- (preferred) int, long, char, etc
-					storageclass = { italic = true }, -- static, register, volatile, etc
-					structure = { italic = true }, -- struct, union, enum, etc
-					parameter = { italic = true }, -- parameter pass in function
-					annotation = { italic = true },
-					tag_attribute = { italic = true }, -- attribute of tag in reactjs
-				},
-				filter = "ristretto", -- classic | octagon | pro | machine | ristretto | spectrum
-				-- Enable this will disable filter option
-				day_night = {
-					enable = false, -- turn off by default
-					day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-					night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
-				},
-				inc_search = "background", -- underline | background
-				background_clear = {
-					-- "float_win",
-					"toggleterm",
-					"telescope",
-					-- "which-key",
-					"renamer",
-					"notify",
-					-- "nvim-tree",
-					-- "neo-tree",
-					-- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-				}, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
-			})
-		end,
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "everforest",
-			background = "soft",
-		},
-	},
+    {
+        "craftzdog/solarized-osaka.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    {
+        "neanias/everforest-nvim",
+        name = "everforest",
+        config = function()
+            ColorMyPencils()
+        end,
+    },
+    { "shaunsingh/nord.nvim" },
+    { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
+    {
+        "loctvl842/monokai-pro.nvim",
+        config = function()
+            require("monokai-pro").setup({
+                transparent_background = false,
+                terminal_colors = true,
+                devicons = true, -- highlight the icons of `nvim-web-devicons`
+                styles = {
+                    comment = { italic = true },
+                    keyword = { italic = true }, -- any other keyword
+                    type = { italic = true }, -- (preferred) int, long, char, etc
+                    storageclass = { italic = true }, -- static, register, volatile, etc
+                    structure = { italic = true }, -- struct, union, enum, etc
+                    parameter = { italic = true }, -- parameter pass in function
+                    annotation = { italic = true },
+                    tag_attribute = { italic = true }, -- attribute of tag in reactjs
+                },
+                filter = "ristretto",   -- classic | octagon | pro | machine | ristretto | spectrum
+                -- Enable this will disable filter option
+                day_night = {
+                    enable = false, -- turn off by default
+                    day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+                    night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+                },
+                inc_search = "background", -- underline | background
+                background_clear = {
+                    -- "float_win",
+                    "toggleterm",
+                    "telescope",
+                    -- "which-key",
+                    "renamer",
+                    "notify",
+                    -- "nvim-tree",
+                    -- "neo-tree",
+                    -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+                }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+            })
+        end,
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "everforest",
+            background = "soft",
+        },
+    },
 }
