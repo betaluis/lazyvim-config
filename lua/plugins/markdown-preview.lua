1ec8c544a5316a4250f3687b16a945b40215598c
Luis Betancourt <luis.betancourt.dev@gmail.com>
2026-05-28 10:07:36 -0500

    default browser for md preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -3,7 +3,7 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     init = function()
-        vim.g.mkdp_browser = "chromium"
+        vim.g.mkdp_browser = ""
         vim.g.mkdp_echo_preview_url = 1
         -- vim.g.mkdp_markdown_css = vim.fn.expand("~/.config/nvim/markdown-preview.css")
     end,
