464d7381d0de79581d3659df27c7607f0e498237
Luis Betancourt <luis.betancourt.dev@gmail.com>
2026-06-17 08:43:54 -0500

    remove colorMyPencils

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,3 @@
-function ColorMyPencils(color)
-    color = color or "everforest"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(1, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(1, "NormalFloat", { bg = "none" })
-end
-
 return {
     {
         "craftzdog/solarized-osaka.nvim",
@@ -22,9 +14,7 @@ return {
     {
         "neanias/everforest-nvim",
         name = "everforest",
-        config = function()
-            ColorMyPencils()
-        end,
+
     },
     { "shaunsingh/nord.nvim" },
     { "ellisonleao/gruvbox.nvim", priority = 1000, config = true, opts = ... },
@@ -37,22 +27,22 @@ return {
                 devicons = true, -- highlight the icons of `nvim-web-devicons`
                 styles = {
                     comment = { italic = true },
-                    keyword = { italic = true }, -- any other keyword
-                    type = { italic = true }, -- (preferred) int, long, char, etc
-                    storageclass = { italic = true }, -- static, register, volatile, etc
-                    structure = { italic = true }, -- struct, union, enum, etc
-                    parameter = { italic = true }, -- parameter pass in function
+                    keyword = { italic = true },       -- any other keyword
+                    type = { italic = true },          -- (preferred) int, long, char, etc
+                    storageclass = { italic = true },  -- static, register, volatile, etc
+                    structure = { italic = true },     -- struct, union, enum, etc
+                    parameter = { italic = true },     -- parameter pass in function
                     annotation = { italic = true },
                     tag_attribute = { italic = true }, -- attribute of tag in reactjs
                 },
-                filter = "ristretto",   -- classic | octagon | pro | machine | ristretto | spectrum
+                filter = "ristretto",                  -- classic | octagon | pro | machine | ristretto | spectrum
                 -- Enable this will disable filter option
                 day_night = {
-                    enable = false, -- turn off by default
-                    day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+                    enable = false,            -- turn off by default
+                    day_filter = "pro",        -- classic | octagon | pro | machine | ristretto | spectrum
                     night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
                 },
-                inc_search = "background", -- underline | background
+                inc_search = "background",     -- underline | background
                 background_clear = {
                     -- "float_win",
                     "toggleterm",
