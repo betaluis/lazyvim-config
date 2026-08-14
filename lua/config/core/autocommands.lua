f3b8652f3c99d0fe0a1379c3002a8491f94601d2
betaluis <luis.betancourt.dev.com>
2026-08-13 20:56:37 -0500

    Adjust netrw h behavior in tree view

--- lua/config/core/autocommands.lua
+++ lua/config/core/autocommands.lua
@@ -1,6 +1,19 @@
 local autocommand = vim.api.nvim_create_autocmd
 local keymap = vim.keymap.set
 
+local function netrw_h()
+	-- In tree view, close/squeeze the directory containing the cursor instead of
+	-- changing the root to the parent directory. In other list styles, keep the
+	-- previous "go up one directory" behavior.
+	if vim.w.netrw_liststyle == 3 then
+		local plug = vim.api.nvim_replace_termcodes("<Plug>NetrwTreeSqueeze", true, false, true)
+		vim.api.nvim_feedkeys(plug, "m", false)
+	else
+		local keys = vim.api.nvim_replace_termcodes("-^", true, false, true)
+		vim.api.nvim_feedkeys(keys, "m", false)
+	end
+end
+
 -- Use 'q' to quit from common plugins
 autocommand({ "Filetype" }, {
 	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
@@ -15,15 +28,12 @@ autocommand({ "Filetype" }, {
 autocommand({ "Filetype" }, {
 	pattern = { "netrw" },
 	callback = function()
-		vim.cmd([[
-            nmap <buffer> H u
-            nmap <buffer> h -^
-            nmap <buffer> l <CR>
-            nmap <buffer> . gh
-            nmap <buffer> P <C-w>z
-            setlocal number
-
-        ]])
+		keymap("n", "H", "u", { buffer = true, remap = true, silent = true })
+		keymap("n", "h", netrw_h, { buffer = true, silent = true, desc = "Close tree dir or go up" })
+		keymap("n", "l", "<CR>", { buffer = true, remap = true, silent = true })
+		keymap("n", ".", "gh", { buffer = true, remap = true, silent = true })
+		keymap("n", "P", "<C-w>z", { buffer = true, remap = true, silent = true })
+		vim.opt_local.number = true
 	end,
 })
 
