335c718f4efe8efddc8bc16687370438f8404abd
Luis Betancourt <luis.betancourt.dev@gmail.com>
2026-08-21 09:23:23 -0500

    update nvim config

---
+++ lua/plugins/markview.lua
@@ -0,0 +1,18 @@
+<<<<<<< Updated upstream
+=======
+-- return {
+--   "OXY2DEV/markview.nvim",
+--   opts = {},
+--   config = function(_, opts)
+--     require("markview").setup(opts)
+--     require("markview.extras.checkboxes").setup()
+--
+--     vim.keymap.set("n", "<leader>c", "<cmd>Checkbox toggle<CR>", {
+--       desc = "Toggle checkbox",
+--     })
+--     vim.keymap.set("x", "<leader>c", ":'<,'>Checkbox toggle<CR>", {
+--       desc = "Toggle checkboxes",
+--     })
+--   end,
+-- }
+>>>>>>> Stashed changes
